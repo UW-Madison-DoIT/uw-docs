8ce4d2ccd3fe2c4f4637a2fd9ec174f9cd423802
Tim Levett <tim.levett@gmail.com>
2016-03-17 11:15:49 -0500

    change name from uw-frame to generic NAMES.title

--- my-app/home/controllers.js
+++ my-app/home/controllers.js
@@ -3,7 +3,7 @@
 define(['angular','require', 'marked'], function(angular, require, marked) {
   var app = angular.module('docs.main.controllers', []);
 
-  app.controller('MarkdownPageController', ['$scope', '$routeParams', function($scope, $routeParams) {
+  app.controller('MarkdownPageController', ['NAMES', '$scope', '$routeParams', function(NAMES, $scope, $routeParams) {
     $scope.title = $routeParams.markdownfilename;
     if($scope.title) {
       $scope.title = $scope.title.replace('-',' ');
